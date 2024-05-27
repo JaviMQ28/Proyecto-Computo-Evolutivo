66b9496869607447d8b385f673d23df1e7e31a92
JaviMQ28 <javier_manceraq@ciencias.unam.mx>
2024-05-26 18:21:30 -0600

    El usuario pone el horario y se agregan operadores geneticos

--- proyecto.py
+++ proyecto.py
@@ -77,9 +77,9 @@ def leer_archivo(ruta_archivo):
 
     return num_nodos, num_camiones, capacidad_camion, coordenadas
 
-def dividir_horas(intervalos):
-    hora_inicio_dt = datetime.strptime("8:00:00", "%H:%M:%S")
-    hora_fin_dt = datetime.strptime("16:00:00", "%H:%M:%S")
+def dividir_horas(intervalos, horario):
+    hora_inicio_dt = datetime.strptime(horario[0], "%H:%M:%S")
+    hora_fin_dt = datetime.strptime(horario[1], "%H:%M:%S")
 
     duracion = hora_fin_dt - hora_inicio_dt
     intervalo_duracion = duracion / intervalos
@@ -99,10 +99,9 @@ exit(0)
 '''
 
 # Función para generar una hora aleatoria
-def generar_hora_aleatoria(hora_inicio):    
-    hora_fin = "16:00:00"
-    hora_inicio_dt = datetime.strptime(hora_inicio, "%H:%M:%S")
-    hora_fin_dt = datetime.strptime(hora_fin, "%H:%M:%S")    
+def generar_hora_aleatoria(horario):       
+    hora_inicio_dt = datetime.strptime(str(horario[0]), "%H:%M:%S")
+    hora_fin_dt = datetime.strptime(str(horario[1]), "%H:%M:%S")    
     diferencia = hora_fin_dt - hora_inicio_dt
     total_segundos = diferencia.total_seconds()
     segundos_aleatorios = random.randint(0, int(total_segundos))
@@ -114,60 +113,6 @@ hora = generar_hora_aleatoria("08:00:00")
 print(hora)
 exit(0)
 '''
-
-# Genera poblacion de soluciones con representacion de conjuntos de permutaciones
-# - solucion[0] = Trayecto del camion
-# - solucion[1] = Horario del camion
-# - solucion[2] = Hora en que finalizo su trabajo el camion
-def genera_poblacion(num_nodos, num_camiones, coordenadas):
-    poblacion = []    
-    deposito = coordenadas[0]
-    i = 0
-    while i < 100:      
-        coordenadas_agregadas = []  
-        solucion = []    
-        periodos = dividir_horas(num_camiones)            
-        nodos = 1
-        camion = 1    
-        hora_inicio = "08:00:00"
-                          
-        while camion <= num_camiones:                                     
-            dimension = num_nodos-1
-            datos_camion = []
-            trayecto = []            
-            num_contenedores = 0            
-            trayecto.append(deposito)
-            coordenadas_agregadas.append(deposito)                
-            if dimension > 0:
-                num_contenedores = random.randint(1, dimension)
-            contenedores = 1
-            while contenedores <= num_contenedores:
-                coordenada = random.choice(coordenadas)
-                if coordenada not in coordenadas_agregadas:                    
-                    trayecto.append(coordenada)
-                    coordenadas_agregadas.append(coordenada)
-                    nodos += 1
-                contenedores += 1                                                 
-            #print(nodos)      
-            if camion == num_camiones and nodos < num_nodos:
-                coordenadas_faltantes = [coordenada for coordenada in coordenadas if coordenada not in coordenadas_agregadas]
-                for coordenada in coordenadas_faltantes:
-                    trayecto.append(coordenada)
-                    nodos += 1
-            #print(nodos)      
-            trayecto.append(deposito) 
-            datos_camion.append(trayecto)
-            datos_camion.append(periodos[camion-1])
-            hora_inicio = generar_hora_aleatoria(hora_inicio)
-            datos_camion.append(hora_inicio)
-            solucion.append(datos_camion)
-            dimension = dimension - contenedores               
-            camion += 1                                       
-        individuo = Individuo(solucion, 0)
-        poblacion.append(individuo)
-        i += 1    
-    
-    return poblacion
 
 def distancia_euclidiana(x, y):
     distancia_x = x[1] - x[0]
@@ -223,16 +168,338 @@ def evaluar_poblacion(poblacion, capacidad):
         poblacion[j] = evaluar_individuo(i, capacidad)
         j += 1
 
+# Genera poblacion de soluciones con representacion de conjuntos de permutaciones
+# - solucion[0] = Trayecto del camion
+# - solucion[1] = Horario del camion
+# - solucion[2] = Hora en que finalizo su trabajo el camion
+def genera_poblacion(num_nodos, num_camiones, coordenadas, horario):
+    poblacion = []    
+    deposito = coordenadas[0]
+    i = 0
+    while i < 100:      
+        coordenadas_agregadas = []  
+        solucion = []    
+        periodos = dividir_horas(num_camiones, horario)            
+        nodos = 1
+        camion = 1    
+        #hora_inicio = horario[0]
+                          
+        while camion <= num_camiones:                                     
+            dimension = num_nodos-1
+            datos_camion = []
+            trayecto = []            
+            num_contenedores = 0            
+            trayecto.append(deposito)
+            coordenadas_agregadas.append(deposito)                
+            if dimension > 0:
+                num_contenedores = random.randint(1, dimension)
+            contenedores = 1
+            while contenedores <= num_contenedores:
+                coordenada = random.choice(coordenadas)
+                if coordenada not in coordenadas_agregadas:                    
+                    trayecto.append(coordenada)
+                    coordenadas_agregadas.append(coordenada)
+                    nodos += 1
+                contenedores += 1                                                 
+            #print(nodos)      
+            if camion == num_camiones and nodos < num_nodos:
+                coordenadas_faltantes = [coordenada for coordenada in coordenadas if coordenada not in coordenadas_agregadas]
+                for coordenada in coordenadas_faltantes:
+                    trayecto.append(coordenada)
+                    nodos += 1
+            #print(nodos)      
+            trayecto.append(deposito) 
+            datos_camion.append(trayecto)
+            datos_camion.append(periodos[camion-1])
+            hora_que_finalizo = generar_hora_aleatoria(periodos[camion-1])
+            datos_camion.append(hora_que_finalizo)
+            solucion.append(datos_camion)
+            dimension = dimension - contenedores               
+            camion += 1                                       
+        individuo = Individuo(solucion, 0)
+        poblacion.append(individuo)
+        i += 1    
+    
+    return poblacion
+
+# Regresa dos individuos de la poblacion que seran los padres
+def seleccion_por_torneo(poblacion, capacidad):
+    seleccionados = []
+    k = 0    
+    # Seleccionamos k individuos de la poblacion
+    while k < 10:
+        individuo = random.choice(poblacion)
+        if individuo not in seleccionados:                    
+            seleccionados.append(individuo)
+        k += 1
+    seleccionados = sorted(seleccionados, key=lambda individuo: evaluar_individuo(individuo, capacidad).evaluacion)    
+    # Obtenemos al mejor de los seleccionados
+    mejor_de_seleccionados_1 = seleccionados[0]
+
+    seleccionados = []
+    k = 0    
+    # Seleccionamos otros k individuos de la poblacion
+    while k < 10:
+        individuo = random.choice(poblacion)
+        if individuo not in seleccionados:                    
+            seleccionados.append(individuo)
+        k += 1
+    seleccionados = sorted(seleccionados, key=lambda individuo: evaluar_individuo(individuo, capacidad).evaluacion)    
+    # Obtenemos al mejor de los seleccionados
+    mejor_de_seleccionados_2 = seleccionados[0]
+    
+    return poblacion.index(mejor_de_seleccionados_1), poblacion.index(mejor_de_seleccionados_2)
+
+# Realiza el cruce para permutaciones, con probabilidad pc => [0.6-0.9], 
+# para los horarios de los hijos uno tendra el horario del primer padre y el otro del segundo,
+# para los trayectos de cada camion de los hijos seran de diferentes tamaños al de los padres
+def cruza_de_permutaciones(sol1, sol2, pc):    
+    trayecto_de_sol1 = []
+    trayecto_de_sol2 = []
+    sol_hijo1 = []
+    sol_hijo2 = []
+    trayecto_de_h1 = []
+    trayecto_de_h2 = []    
+    
+    # Recupera el trayecto de los camiones a los contenedores, sin contar al deposito, de la primera solucion
+    for camion in sol1:
+        trayecto = camion[0]
+        deposito = trayecto[0]
+        contenedor = 1
+        while trayecto[contenedor] != deposito:
+            trayecto_de_sol1.append(trayecto[contenedor])
+            contenedor += 1
+    '''
+    print(trayecto_de_sol1)
+    '''
+    # Recupera el trayecto de los camiones a los contenedores, sin contar al deposito, de la segunda solucion
+    for camion in sol2:
+        trayecto = camion[0]
+        deposito = trayecto[0]
+        contenedor = 1
+        while trayecto[contenedor] != deposito:
+            trayecto_de_sol2.append(trayecto[contenedor])
+            contenedor += 1
+    '''
+    print()
+    print(trayecto_de_sol2)
+    '''
+    tamanio = len(trayecto_de_sol1)
+    indice_comienzo = random.randrange(0, tamanio)
+    indice_final = random.randrange(indice_comienzo, tamanio)
+    indices_no_agregados = []
+    ind = 0
+    while ind < tamanio:
+        if ind >= indice_comienzo and ind <= indice_final:
+            if np.random.rand() <= pc:
+                trayecto_de_h1.append(trayecto_de_sol1[ind])
+                trayecto_de_h2.append(trayecto_de_sol2[ind])
+            else:
+                trayecto_de_h1.append(None)
+                trayecto_de_h2.append(None)
+                indices_no_agregados.append(ind)
+        else:            
+            trayecto_de_h1.append(None)
+            trayecto_de_h2.append(None)         
+            indices_no_agregados.append(ind)   
+        ind += 1    
+    '''
+    print()
+    print(trayecto_de_h1)
+    print()
+    print(trayecto_de_h2)
+    '''
+    for indice in indices_no_agregados:
+        if trayecto_de_h1[indice] == None and trayecto_de_h2[indice] == None:
+            ind = 0
+            while ind < len(trayecto_de_sol1):
+                if trayecto_de_sol2[ind] not in trayecto_de_h1:
+                    trayecto_de_h1[indice] = trayecto_de_sol2[ind]
+                ind += 1
+            ind = 0
+            while ind < len(trayecto_de_sol1):
+                if trayecto_de_sol1[ind] not in trayecto_de_h2:
+                    trayecto_de_h2[indice] = trayecto_de_sol1[ind]            
+                ind += 1            
+    '''
+    print()
+    print(trayecto_de_h1)
+    print()
+    print(trayecto_de_h2)
+    print()
+    '''    
+    num_camiones = len(sol1)
+    num_nodos = len(trayecto_de_h1) + 1
+    sol_hijo1 = []
+    camion = 1
+    nodos = 1
+    while camion <= num_camiones:
+        dimension = len(trayecto_de_h1)-1
+        datos_camion = []
+        trayecto_h1 = []
+        trayecto_h1.append(deposito)              
+        if dimension > 0:
+            num_contenedores = random.randint(1, dimension)
+        contenedores = 1
+        while contenedores < num_contenedores:
+            contenedor = trayecto_de_h1[0]
+            trayecto_h1.append(contenedor)            
+            trayecto_de_h1.remove(contenedor)
+            contenedores += 1                                                 
+        
+        if camion == num_camiones and nodos < num_nodos:
+            contenedores_faltantes = [coordenada for coordenada in trayecto_de_h1 if coordenada not in trayecto_h1]
+            for contenedor in contenedores_faltantes:
+                trayecto_h1.append(contenedor)
+                trayecto_de_h1.remove(contenedor)
+                nodos += 1
+        
+        trayecto_h1.append(deposito) 
+        datos_camion.append(trayecto_h1)
+        datos_camion.append(sol1[camion-1][1])
+        hora_que_finalizo = generar_hora_aleatoria(sol1[camion-1][1])
+        datos_camion.append(hora_que_finalizo)
+        sol_hijo1.append(datos_camion)
+        dimension = dimension - contenedores               
+        camion += 1  
+    '''
+    print(sol_hijo1)
+    print(trayecto_de_h1)
+    '''
+    ind1 = Individuo(sol_hijo1, 0)
+
+    num_camiones = len(sol2)
+    num_nodos = len(trayecto_de_h2) + 1
+    sol_hijo2 = []
+    camion = 1
+    nodos = 1
+    while camion <= num_camiones:
+        dimension = len(trayecto_de_h2)-1
+        datos_camion = []
+        trayecto_h2 = []
+        trayecto_h2.append(deposito)              
+        if dimension > 0:
+            num_contenedores = random.randint(1, dimension)
+        contenedores = 1
+        while contenedores < num_contenedores:
+            contenedor = trayecto_de_h2[0]
+            trayecto_h2.append(contenedor)            
+            trayecto_de_h2.remove(contenedor)
+            contenedores += 1                                                 
+        
+        if camion == num_camiones and nodos < num_nodos:
+            contenedores_faltantes = [coordenada for coordenada in trayecto_de_h2 if coordenada not in trayecto_h2]
+            for contenedor in contenedores_faltantes:
+                trayecto_h2.append(contenedor)
+                trayecto_de_h2.remove(contenedor)
+                nodos += 1
+        
+        trayecto_h2.append(deposito) 
+        datos_camion.append(trayecto_h2)
+        datos_camion.append(sol2[camion-1][1])
+        hora_que_finalizo = generar_hora_aleatoria(sol2[camion-1][1])
+        datos_camion.append(hora_que_finalizo)
+        sol_hijo2.append(datos_camion)
+        dimension = dimension - contenedores               
+        camion += 1  
+    '''
+    print(sol_hijo2)
+    print(trayecto_de_h2)
+    '''
+    ind2 = Individuo(sol_hijo2, 0)
+
+    return ind1, ind2
+
+# Realiza la mutacion de un individuo, con probabilidad pc => [0.01-0.1]
+def mutacion(solucion, pm):
+    trayecto_de_sol = []
+    # Recupera el trayecto de los camiones a los contenedores, sin contar al deposito
+    for camion in solucion:
+        trayecto = camion[0]
+        deposito = trayecto[0]
+        contenedor = 1
+        while trayecto[contenedor] != deposito:
+            trayecto_de_sol.append(trayecto[contenedor])
+            contenedor += 1
+    '''
+    print(trayecto_de_sol)    
+    print()
+    '''
+    i = 0
+    while i < len(trayecto_de_sol):
+        if np.random.rand() <= pm:
+            temp = trayecto_de_sol[i]
+            pos_random = random.randint(i, len(trayecto_de_sol)-1)
+            trayecto_de_sol[i] = trayecto_de_sol[pos_random]
+            trayecto_de_sol[pos_random] = temp                         
+        i += 1
+    '''    
+    print(trayecto_de_sol)
+    print()
+    '''
+    deposito = solucion[0][0][0]
+    camion = 0
+    ind = 0
+    while camion < len(solucion):
+        i = 0
+        while i < len(solucion[camion][0]):              
+            if solucion[camion][0][i] != deposito:
+                solucion[camion][0][i] = trayecto_de_sol[ind]
+                ind += 1
+            i += 1     
+        camion += 1   
+    #print(solucion)
+
+    mut = Individuo(solucion,0)
+    
+    return mut
+
+def alg_NSGA2(num_nodos, num_camiones, coordenadas, horario):
+    # Inicializa poblacion
+    poblacion = genera_poblacion(int(num_nodos), int(num_camiones), coordenadas, horario)
+    #print(f'\nPoblación generada: \n{poblacion}')
+
+    # Evaluacion de la poblacion
+    evaluar_poblacion(poblacion, int(capacidad))
+    #print(f'\nEvaluación: \n{poblacion}\n')
+
+    # Condicion de termino
+    #for generacion in range(num_gen): 
+    # Seleccion por torneo de padres    
+    padre1, padre2 = seleccion_por_torneo(poblacion, int(capacidad))
+    #print(poblacion[padre1].solucion)
+    #print(poblacion[padre2].solucion)
+    #print()
+
+    # Cruza de permutaciones
+    hijo1, hijo2 = cruza_de_permutaciones(poblacion[padre1].solucion, poblacion[padre2].solucion, random.uniform(0.6, 0.9))
+    #print(hijo1.solucion)
+    #print(hijo2.solucion)
+
+    # Mutacion de los hijos
+    hijo1 = mutacion(hijo1.solucion, random.uniform(0.01, 0.1))
+    hijo2 = mutacion(hijo2.solucion, random.uniform(0.01, 0.1))
+
 if __name__ == '__main__':
     ruta_archivo = sys.argv[1]
     numN, numC, capacidad, coordenadas = leer_archivo(ruta_archivo)
     print(f"\nCantidad de nodos: {numN}")
     print(f"Cantidad de camiones: {numC}")
     print(f"Capacidad de cada camión: {capacidad}")
-    print(f"Coordenadas: \n{coordenadas}")
+    print(f"Coordenadas: \n{coordenadas}\n")
 
-    poblacion = genera_poblacion(int(numN), int(numC), coordenadas)
-    #print(f'\nPoblación generada: \n{poblacion}')
+    horario_establecido = False
+    while not horario_establecido:
+        try:
+            hora_inicio = input("Ingresa la hora de inicio, en formato de 24 hrs y que tenga la siguiente forma: HH:MM:SS\n")
+            hora_fin = input("Ingresa la hora final, en formato de 24 hrs y que tenga la siguiente forma: HH:MM:SS\n")
 
-    evaluar_poblacion(poblacion, int(capacidad))
-    print(f'\nEvaluación: \n{poblacion}\n')
+            if time.fromisoformat(hora_inicio) > time.fromisoformat(hora_fin):
+                print("ERROR: Ingresa un horario correcto.")
+            else:
+                horario_establecido = True
+        except ValueError:
+            print("ERROR: Ingresa el horario con el formato que se indica.")
+
+    horario = [hora_inicio, hora_fin]
+    alg_NSGA2(numN, numC, coordenadas, horario)    
